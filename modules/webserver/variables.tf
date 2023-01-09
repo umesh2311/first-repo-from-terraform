bdc4cddc57a0154b41a8d0be4629f8bafb0be281
Umesh Joshi <joshi.umesh@live.in>
2023-01-09 20:31:25 +0530

    fixed variables.tf

--- modules/webserver/variables.tf
+++ modules/webserver/variables.tf
@@ -1,18 +1,15 @@
 variable "instance_type" {
   type = string
-}
-variable "access_key" {
-  type = string
-}
-variable "secret_key" {
-  type = string
+  default = "t2.micro"
 }
 variable "image_id" {
   type = string
+  default = "ami-062df10d14676e201"
 }
-variable "key" {
+/*variable "key" {
   type = string
 }
 variable "key_name" {
   type = string
 }
+*/
